009f82129acabd9116a19a0ed2892f85553aed61
wesleymadeira <wesleygabrielmadeira2022@gmail.com>
2026-08-08 17:24:38 -0300

    exclusão de arquivos e começo da página home

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,16 +1,35 @@
 <template>
   <main>
-    <div class="hero">
-      <div class="hero-texto">
-      <h1>Olá, café</h1>
+    <section class="hero">
+      <div class="hero-icon">
+        ☕
       </div>
-      <div class="hero-imagem">
-        <img src="@/assets/img/images.jpg" alt="imagem">
+      <div class="title">
+        <h1>COFFEE QUALITY CHALLENGE</h1>
+        <h2>Campeonato de Avaliação Sensorial de Cafés Especiais</h2>
+        <p>
+          Avalie, compare e descubra os melhores cafés
+          através dos seus sentidos.
+        </p>
       </div>
-    </div>
-    <button class="button">
-      <RouterLink to="/ranking" class="text-button">ver ranking</RouterLink>
-    </button>
+      <div class="cards">
+        <div class="card">
+          <div class="icone">👥</div>
+          <h3>Cafés Participantes</h3>
+          <span>4</span>
+        </div>
+        <div class="card">
+          <div class="icone">📋</div>
+          <h3>Avaliações Registradas</h3>
+          <span>0</span>
+        </div>
+      </div>
+      <button class="btn-hero">
+        <RouterLink to="/cafes">
+        ☕ Ver Cafés
+        </RouterLink>
+      </button>
+    </section>
   </main>
 </template>
 
@@ -18,30 +37,68 @@
 </script>
 
 <style scoped>
-.hero{
-  background: #ffffff;
+main {
+  background-color: #fff;
 }
 
-.hero-texto{
+a{
+  text-decoration: none;
+  color: #fff;
+}
+
+.hero-icon {
   color: black;
+  font-size: 65px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
 }
 
-.button{
-  text-decoration: none;
-  background-color: #6f4327;
+.title {
+  color: black;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+}
+
+.title h1 {
+  font-size: 3.1rem;
+}
+
+.title h2 {
+  font-size: 1.5rem;
+}
+
+.cards {
+  display: flex;
+  justify-content: center;
+  gap: 30px;
+  margin: 50px 0;
+  flex-wrap: wrap;
+}
+
+.card {
+  color: black;
+  width: 280px;
+  background: #fff;
+  border: 1px solid #e7e7e7;
+  border-radius: 15px;
+  padding: 30px;
+  box-shadow: 0 5px 15px rgba(0, 0, 0, .05);
+}
+
+.btn-hero {
+  display: flex;
+  justify-content: center;
+  align-items: center;
   border: none;
-  border-radius: 10px;
-  padding: 1vw 2vw;
-  cursor: pointer;
-}
-
-.button:hover{
-  background-color: #5d3720;
-}
-
-.text-button{
-  text-decoration: none;
+  border-radius: 8px;
+  background: #6f4327;
   color: white;
+  font-size: 22px;
+  cursor: pointer;
+  transition: .3s;
 }
 
 </style>
